133df1c3f1bcbc71af37c4782241c7e3aaa486bf
Landon Hemsley <landonhemsley@proton.me>
2026-07-06 11:18:59 -0600

    save an instance refresh trigger into my zpresto

--- aws.zsh
+++ aws.zsh
@@ -27,3 +27,38 @@ tunnel() {
 ww () {
     watch -n 1 "aws ecs describe-services --cluster hound_cluster --service ${1} --query 'services[0].events[].message' --output table"
 }
+
+# trigger an ec2 rotation
+asg_refresh() {
+  if [[ -z "${AWS_ACCESS_KEY_ID}" || -z "${AWS_SECRET_ACCESS_KEY}" || -z "${AWS_SESSION_TOKEN}" ]]; then
+    echo "must assume the aws role first. source assumeRole"
+    return 1
+  fi
+
+  local prefs_json="{ \"AutoScalingGroupName\": \"crzr_asg\", \
+    \"Preferences\": {
+      \"MinHealthyPercentage\": 100, \
+      \"InstanceWarmup\": 0, \
+      \"SkipMatching\": true, \
+      \"AutoRollback\": false, \
+      \"ScaleInProtectedInstances\": \"Ignore\", \
+      \"StandbyInstances\": \"Ignore\", \
+      \"MaxHealthyPercentage\": 110 } \
+    }"
+
+  local cmd="aws autoscaling start-instance-refresh --cli-input-json '${prefs_json}'"
+  resp=$(eval "${cmd}")
+  exit_code=$?
+
+  if [[ "$exit_code" != "0" ]]; then
+    echo "failed to start instance refresh."
+    return 1
+  fi
+
+  local id=$(echo "${resp}" | jq '.InstanceRefreshId')
+
+  watch -n 10 "aws autoscaling describe-instance-refreshes \
+    --auto-scaling-group-name crzr_asg \
+    --query \"InstanceRefreshes[?InstanceRefreshId=='${id}'].{status: Status, PctComplete: PercentageComplete}\" \
+    --output table"
+}
